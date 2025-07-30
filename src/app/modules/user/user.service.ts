53ac30820b481bdaca21a9b1f23a4745fbee9af2
Monir <mrmonir0558@gmail.com>
2025-07-30 17:51:08 +0600

    Created Login Api

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,24 +2,32 @@ import httpStatus from 'http-status-codes';
 import AppError from '../../errorHelpers/AppError';
 import { IAuthProvider, IUser } from './user.interface';
 import User from './user.model';
+import bcryptjs from 'bcryptjs';
 
 // create user
 const createUser = async (payload: Partial<IUser>) => {
-    const { email, ...rest } = payload;
+    const { email, password, ...rest } = payload;
 
     const isUserExist = await User.findOne({ email });
 
     if (isUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, 'User Already Exist');
     }
+    // Hash the password if provided
+    const hashedPassword = password ? await bcryptjs.hash(password as string, 10) : undefined;
+
+
+    // Check if password is provided and hashed
+    // const isPasswordMatched = await bcryptjs.compare(password as string, hashedPassword as string)
 
     const authProvider: IAuthProvider = {
         provider: 'credentials',
         providerId: email as string
     };
-    
+
     const user = await User.create({
         email,
+        password: hashedPassword,
         auths: [authProvider],
         ...rest
     });
