708c26808dfd3acef12d9c459213f30dc2dfbb0c
Monir <mrmonir0558@gmail.com>
2025-08-01 16:47:39 +0600

    set accesstoken and refreshtoken in the cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,12 +3,20 @@ import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
 import { catchAsync } from '../../utils/catchAsync';
 import { sendResponse } from '../../utils/sendResponse';
-import { UserServices } from '../user/user.service';
 import { AuthServices } from './auth.service';
+import AppError from '../../errorHelpers/AppError';
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body);
 
+    res.cookie('accessToken', loginInfo.accessToken, {
+        httpOnly: true,
+        secure: false,
+    })
+    res.cookie('refreshToken', loginInfo.refreshToken, {
+        httpOnly: true,
+        secure: false,
+    })
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
@@ -17,6 +25,22 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
     });
 });
 
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No refresh token provided from cookies");
+    }
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'User Logged In Successfully',
+        data: tokenInfo
+    });
+});
+
 export const AuthControllers = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken
 };
